10064d9b3b59858b084309987cc9d1e9cae945d6
Your name <hoan42532@gmail.com>
2022-05-03 09:57:46 +0700

    add link to products

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import numpy as np
-from PIL import Image
+from PIL import Image,ExifTags
 from feature_extractor import FeatureExtractor
 from datetime import datetime
 from flask import Flask, request, render_template
@@ -11,9 +11,19 @@ app = Flask(__name__)
 fe = FeatureExtractor()
 features = []
 img_paths = []
+img_names = []
+img_urls = []
 for feature_path in Path("./static/feature").glob("*.npy"):
     features.append(np.load(feature_path))
     img_paths.append(Path("./static/img") / (feature_path.stem + ".jpg"))
+    img_names.append(feature_path.stem)
+    a = feature_path.stem.rpartition('.')[0].rpartition('.')
+    img_urls.append("localhost:3000/" + a[0] + "/" + a[2])
+    # print("feature_path.stem: ",feature_path.stem)
+    # print("a: ",a)
+    # print("a[0]: ",a[0])
+    # print("a[2]: ",a[2])
+
 features = np.array(features)
 
 
@@ -27,15 +37,21 @@ def index():
         uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + file.filename
         img.save(uploaded_img_path)
 
+       
+                
         # Run search
         query = fe.extract(img)
         dists = np.linalg.norm(features-query, axis=1)  # L2 distances to features
-        ids = np.argsort(dists)[:30]  # Top 30 results
-        scores = [(dists[id], img_paths[id]) for id in ids]
+        ids = np.argsort(dists)[:15]  # Top 15 results  (array ids of image in img folder)
+        scores = [(dists[id], img_paths[id], img_names[id], img_urls[id]) for id in ids]   # array save accurate and url/path of image
+        
+        # exif_data = img._getexif()
 
         return render_template('index.html',
                                query_path=uploaded_img_path,
-                               scores=scores)
+                               scores=scores,
+                               img_urls=img_urls,
+                               )
     else:
         return render_template('index.html')
 
